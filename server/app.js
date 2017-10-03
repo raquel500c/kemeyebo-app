a08598df0b98f90f518793f0cb71a064ea0b9ca4
raquel500c <raquel500c@gmail.com>
2017-10-03 15:23:32 +0200

    auth server verified with postman

--- server/app.js
+++ server/app.js
@@ -1,22 +1,37 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const session    = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const logger = require('morgan');
+const passport   = require('passport');
 const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const mongoose     = require('mongoose');
-
-
-mongoose.connect('mongodb://localhost/server');
+const bodyParser = require('body-parser');
+const debug = require('debug')("angularauth:"+path.basename(__filename).split('.')[0]);
+const authRoutes = require('./routes/auth');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+require('./config/database');
+
+var whitelist = [
+    'http://localhost:4200',
+];
+var corsOptions = {
+    origin: function(origin, callback){
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    },
+    credentials: true
+};
+app.use(cors(corsOptions));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,22 +41,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const index = require('./routes/index');
-app.use('/', index);
+app.use(session({
+  secret: 'angular auth passport secret shh',
+  resave: true,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 60*60*24*365 },
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+require('./passport/serializers');
+require('./passport/local');
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/auth', authRoutes);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.error(err);
   // render the error page
   res.status(err.status || 500);
   res.render('error');
