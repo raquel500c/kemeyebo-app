9bf1fe9f94046476645f4f35d0c366167dc0ef35
raquel500c <raquel500c@gmail.com>
2017-10-13 10:27:19 +0200

    something to upload to Heroku

--- server/app.js
+++ server/app.js
@@ -39,8 +39,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//DEPLOY-->
-// app.use((req, res, next) => {res.sendfile(__dirname + '/public/index.html')});
 
 app.use(session({
   secret: 'angular auth passport secret shh',
@@ -57,6 +55,9 @@ app.use(passport.session());
 
 app.use('/auth', authRoutes);
 app.use('/', routes);
+app.all('/*', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
