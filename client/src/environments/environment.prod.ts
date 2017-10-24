377ff69e0b1236a02827135196571f76b5e1293c
raquel500c <raquel500c@gmail.com>
2017-10-24 21:35:24 +0200

    config for local -4200

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
+// export const environment = {
+//   production: false,
+//   BASEURL: 'http://localhost:3000'
+// };
+
+//local-4200-->
 export const environment = {
-  production: false,
-  BASEURL: 'http://localhost:3000'
+  production: true,
+  BASEURL: ''
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,10 @@
+// export const environment = {
+//   production: true,
+//   BASEURL: ''
+// };
+
+////local-4200-->
 export const environment = {
-  production: true,
-  BASEURL: ''
+  production: false,
+  BASEURL: 'http://localhost:3000'
 };
