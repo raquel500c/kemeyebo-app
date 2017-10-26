60ee564a0853ebb87b676b70b7c0c96d894cbf9a
raquel500c <raquel500c@gmail.com>
2017-10-26 04:26:08 +0200

    router navigate en signup

--- client/src/app/signupform/signupform.component.ts
+++ client/src/app/signupform/signupform.component.ts
@@ -24,7 +24,7 @@ formInfo = {
       console.log(`Signup with ${username} ${password}`)
       this.auth.signup(username, password)
       .map(user => console.log(user))
-      .subscribe((user) => this.router.navigate(['/']))
+      .subscribe((user) => this.router.navigate(['/articles']));
     } else{
       console.log("You must set a username and a password");
     }
